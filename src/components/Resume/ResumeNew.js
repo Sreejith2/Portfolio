9618764641b590d9dbbbea1dc6e059e09d77b10f
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2024-12-29 08:59:43 +0530

    updated resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/SREEJITH_P_RESUME_LC_Min.pdf";
+import pdf from "../../Assets/SREEJITH_P_RESUME_LC_Min.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
